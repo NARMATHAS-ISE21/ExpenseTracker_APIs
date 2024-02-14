2cd4459949422837d3a725f1f823e1c4a9ce8ba5
naru <narmatha.se21@bitsathy.ac.in>
2024-02-14 14:43:39 +0530

    change

--- server.cjs
+++ server.cjs
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const {ObjectId} = require('mongodb')
 const {connectToDb, getdb} = require('./dbconnection.cjs')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 let db
 connectToDb(function(error){
